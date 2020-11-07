2293586d474f4541cb692bc398f7b46845cd60dc
M1NH42 <minhazalam365@gmail.com>
2020-11-07 21:44:20 +0530

    Add info

--- py-src/built_in.py
+++ py-src/built_in.py
@@ -1,2 +1,5 @@
 # hands on some of the important and most frequently used built-in functions
 # 1 range(lower_limit, upper_limit)
+# 2 range(start, stop, step)
+
+# we can use range in number of different ways
