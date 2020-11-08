bd80941bf96c04c6e65b680b4c37589e8ab9be16
M1NH42 <minhazalam365@gmail.com>
2020-11-08 15:16:42 +0530

    Enumerate(): indexed list

--- py-src/built_in.py
+++ py-src/built_in.py
@@ -15,5 +15,31 @@ nums_list = list(nums)
 print(nums_list)
 
 # Create a new list of odd numbers from 1 to 11 by unpacking a range object
-nums_list2 = [*range(1,12,2)]
+nums_list2 = [*range(1, 12, 2)]
 print(nums_list2)
+
+
+# ENUMERATE()
+
+names = ['Jerry', 'Kramer', 'Elaine', 'George', 'Newman']
+
+# using for loop
+indexed_names = []
+for i in range(len(names)):
+    index_name = (i, names[i])
+    indexed_names.append(index_name)
+
+# Rewrite the for loop to use enumerate
+indexed_names = []
+for i, name in enumerate(names):
+    index_name = (i, name)
+    indexed_names.append(index_name)
+print(indexed_names)
+
+# Rewrite the above for loop using list comprehension
+indexed_names_comp = [(i, name) for i, name in enumerate(names)]
+print(indexed_names_comp)
+
+# Unpack an enumerate object with a starting index of one
+indexed_names_unpack = [*enumerate(names, 1)]
+print(indexed_names_unpack)
