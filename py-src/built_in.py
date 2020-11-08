2260690b0dfe5f6ca0b67116e7931dd14f2d17c5
M1NH42 <minhazalam365@gmail.com>
2020-11-08 16:05:04 +0530

    numpy arrays

--- py-src/built_in.py
+++ py-src/built_in.py
@@ -58,3 +58,21 @@ names_uppercase = [*(names_map)]
 
 # Print the list created above
 print(names_uppercase)
+
+
+# NUMPY
+# Create a list of arrival times
+arrival_times = [*range(10, 60, 10)]
+
+# Convert arrival_times to an array and update the times
+arrival_times_np = np.array(arrival_times)
+new_times = arrival_times_np - 3
+
+# Use list comprehension and enumerate to pair guests to new times
+guest_arrivals = [(names[i], time) for i, time in enumerate(new_times)]
+
+# Map the welcome_guest function to each (guest,time) pair
+welcome_map = map(welcome_guest, guest_arrivals)
+
+guest_welcomes = [*welcome_map]
+print(*guest_welcomes, sep='\n')
